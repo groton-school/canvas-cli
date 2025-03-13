db1373a580ba0a927ec4e0a7a67abf55d5b9fafb
Seth Battis <sbattis@groton.org>
2025-03-13 08:18:53 -0400

    feat(oauth2-cli): rate-limit API requests to limit

--- packages/oauth2-cli/src/index.ts
+++ packages/oauth2-cli/src/index.ts
@@ -2,6 +2,7 @@
 import nodeFetch, { RequestInfo, RequestInit } from 'node-fetch';
 import path from 'node:path';
 import * as OAuth2 from 'oauth2-cli';
+import PQueue from 'p-queue';
 
 export type Credentials = Omit<
   OAuth2.Credentials,
@@ -14,9 +15,11 @@ export class Canvas {
   private client: OAuth2.Client;
   private token?: OAuth2.Token;
   private instance_url: string;
+  private queue: PQueue;
 
   public constructor({ instance_url, ...credentials }: Credentials) {
     this.instance_url = instance_url;
+    this.queue = new PQueue();
     this.client = new OAuth2.Client({
       ...credentials,
       authorization_endpoint: path.join(
@@ -33,18 +36,24 @@ export class Canvas {
   }
 
   public async fetch(endpoint: URL | RequestInfo, init?: RequestInit) {
-    await this.getToken();
-    if (!this.token) {
-      throw new Error('No access token');
-    }
-    return await (
-      await nodeFetch(new URL(endpoint, this.instance_url), {
-        ...init,
-        headers: {
-          ...init?.headers,
-          Authorization: `Bearer ${this.token.access_token}`
+    // TODO monitor quota usage and add delays as necessary
+    // TODO retry failed requests due to quota limits
+    return await this.queue.add(
+      (async () => {
+        await this.getToken();
+        if (!this.token) {
+          throw new Error('No access token');
         }
-      })
-    ).json();
+        return await (
+          await nodeFetch(new URL(endpoint, this.instance_url), {
+            ...init,
+            headers: {
+              ...init?.headers,
+              Authorization: `Bearer ${this.token.access_token}`
+            }
+          })
+        ).json();
+      }).bind(this)
+    );
   }
 }
