117cde622c60b928e3dd41c19e37b752037e147d
Seth Battis <sbattis@groton.org>
2025-03-20 09:59:06 -0400

    feat(canvas-import): convert RSS Reader to Announcements External Feed

---
+++ packages/canvas-import/README.md
@@ -0,0 +1,11 @@
+# @msar/canvas-import
+
+[![npm version](https://badge.fury.io/js/@msar%2Fcanvas-import.svg)](https://badge.fury.io/js/@msar%2Fcanvas-import)
+
+## Notes
+
+- Assignments --> Assignments
+- Bulletin Board --> Page (set to front page, with default course view defaulting to Pages)
+  - RSS Reader --> Announcements External Feed (not shown on page)
+- Topic --> Page
+- All files uploaded to `Imported Files` subfolder of course files
--- packages/canvas-import/src/Snapshot/PodiumPage.ts
+++ packages/canvas-import/src/Snapshot/PodiumPage.ts
@@ -1,7 +1,11 @@
+import { Colors } from '@battis/qui-cli.colors';
+import { Log } from '@battis/qui-cli.log';
 import { JSONValue } from '@battis/typescript-tricks';
 import * as Canvas from '@groton/canvas-types';
+import * as Imported from '@msar/types.import';
 import ejs from 'ejs';
 import path from 'node:path';
+import { Preferences } from '../App/index.js';
 import * as Files from './Files.js';
 
 type ToCanvasArgsOptions = {
@@ -20,13 +24,59 @@ export async function toCanvasArgs({
   front_page = false
 }: ToCanvasArgsOptions): Promise<Canvas.Pages.Parameters> {
   for (const i in body) {
-    body[i] = await Files.uploadLocalFiles({ course, entry: body[i] });
+    const uploaded = (await Files.uploadLocalFiles({
+      course,
+      entry: body[i]
+    })) as Imported.ContentItem.Any;
+    if (
+      'ContentType' in uploaded &&
+      typeof uploaded.ContentType === 'object' &&
+      uploaded.ContentType !== null &&
+      'Content' in uploaded.ContentType &&
+      uploaded.ContentType.Content === 'RSS Reader' &&
+      'Content' in uploaded &&
+      typeof uploaded.Content === 'object' &&
+      uploaded.Content !== null &&
+      'Url' in uploaded.Content &&
+      typeof uploaded.Content.Url === 'string'
+    ) {
+      const args: Canvas.AnnouncementExternalFeeds.Parameters = {
+        url: uploaded.Content.Url,
+        verbosity: 'truncate'
+      };
+      let processed = false;
+      if ('canvas' in uploaded && Preferences.duplicates() === 'update') {
+        if (
+          uploaded.canvas &&
+          'args' in uploaded.canvas &&
+          Imported.isEqual(args, uploaded.canvas?.args)
+        ) {
+          Log.info(`External feed ${Colors.url(args.url)} is up-to-date`);
+          processed = true;
+        }
+      }
+      if (!processed) {
+        const feed = await Canvas.AnnouncementExternalFeeds.create({
+          course,
+          args
+        });
+        if (feed) {
+          (uploaded as Imported.ContentItem.RSSReader).canvas = {
+            id: feed.id,
+            args,
+            created_at: feed.created_at
+          };
+        }
+      }
+    }
+    body[i] = uploaded;
   }
   return {
     'wiki_page[title]': title,
     'wiki_page[body]': await ejs.renderFile(
       path.join(import.meta.dirname, 'PodiumPage.ejs'),
       {
+        course_id: course.id,
         page: body,
         layout
       }
