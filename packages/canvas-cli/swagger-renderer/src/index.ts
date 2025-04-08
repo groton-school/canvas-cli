d13243bb3ca9f431448f1dc426f4dfd01dfb7685
Seth Battis <sbattis@groton.org>
2025-04-08 14:47:44 -0400

    refactor(canvas-cli.swagger-renderer): rename Generate to Render

--- packages/canvas-cli/swagger-renderer/src/index.ts
+++ packages/canvas-cli/swagger-renderer/src/index.ts
@@ -1,11 +1,11 @@
 import { Core } from '@battis/qui-cli.core';
 import { register } from '@battis/qui-cli.plugin';
 import { Root } from '@battis/qui-cli.root';
-import * as Generate from './Generate/index.js';
+import * as Render from './Render/index.js';
 
 Root.configure({root: process.cwd()});
 
 console.log(process.cwd());
 
-await register(Generate);
+await register(Render);
 await Core.run();
