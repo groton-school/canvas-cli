a0300f256013503d15205e068196973631450481
Seth Battis <sbattis@groton.org>
2025-04-15 15:42:18 -0400

    fix(canvas-cli.client): access token correctly accessed

--- packages/canvas-cli/client/src/Client.ts
+++ packages/canvas-cli/client/src/Client.ts
@@ -1,68 +1,73 @@
 import { Canvas } from '@oauth2-cli/canvas';
 // TODO replace node-fetch dependency with native fetch when bumping to node@>=21
+import { JSONObject, JSONValue } from '@battis/typescript-tricks';
 import nodeFetch, { RequestInfo, RequestInit } from 'node-fetch';
 import PQueue from 'p-queue';
 import { isError, stringify } from './Utilities/index.js';
-import { JSONObject, JSONValue } from '@battis/typescript-tricks';
 
 type RequestInitParams = RequestInit & {
-    pathParams?: JSONObject;
-    searchParams?: JSONObject;
-    params?: JSONObject;
-  };
-  type RequestInitMethod = Omit<RequestInitParams, 'method'>;
-  
+  pathParams?: JSONObject;
+  searchParams?: JSONObject;
+  params?: JSONObject;
+};
+type RequestInitMethod = Omit<RequestInitParams, 'method'>;
 
 export class Client extends Canvas {
-    private queue = new PQueue();
-  
-    public async fetch(endpoint: URL | RequestInfo, init?: RequestInit) {
-      const result = await this.queue.add(
-        (async () => {
-          return await nodeFetch(new URL(endpoint, this.instance_url), {
-            ...init,
-            headers: {
-              ...init?.headers,
-              Authorization: `Bearer ${await this.getToken()}`
-            }
-          });
-        }).bind(this)
-      );
-      if (result) {
-        return result;
-      } else {
-        throw new Error(`No result from fetch`);
-      }
+  private queue = new PQueue();
+
+  public async fetch(endpoint: URL | RequestInfo, init?: RequestInit) {
+    const result = await this.queue.add(
+      (async () => {
+        return await nodeFetch(new URL(endpoint, this.instance_url), {
+          ...init,
+          headers: {
+            ...init?.headers,
+            Authorization: `Bearer ${(await this.getToken())?.access_token}`
+          }
+        });
+      }).bind(this)
+    );
+    if (result) {
+      return result;
+    } else {
+      throw new Error(`No result from fetch`);
     }
-  
-    public async fetchAs<T = JSONValue>(
-      endpoint: URL | RequestInfo,
-      { pathParams, searchParams, params, ...init }: RequestInitParams = {}
-    ): Promise<T> {
-      // TODO monitor quota usage and add delays as necessary
-      // TODO retry failed requests due to quota limits
-      let result: T | undefined = undefined;
-      let next: string | undefined = endpoint.toString();
-      endpoint = endpoint.toString();
-      if (pathParams) {
-        endpoint = Object.keys(pathParams || {}).reduce((endpoint, paramName) => {
+  }
+
+  public async fetchAs<T = JSONValue>(
+    endpoint: URL | RequestInfo,
+    { pathParams, searchParams, params, ...init }: RequestInitParams = {}
+  ): Promise<T> {
+    // TODO monitor quota usage and add delays as necessary
+    // TODO retry failed requests due to quota limits
+    let nextEndpoint: string | undefined = endpoint.toString();
+    let result: T | undefined = undefined;
+    if (pathParams) {
+      nextEndpoint = Object.keys(pathParams || {}).reduce(
+        (populatedEndpoint, paramName) => {
           if (pathParams[paramName]) {
-            return endpoint.replaceAll(
+            return populatedEndpoint.replaceAll(
               `{${paramName}}`,
               pathParams[paramName].toString()
             );
           }
-          return endpoint;
-        }, endpoint);
-      }
-      if (params) {
-        init.body = new URLSearchParams(stringify(params));
-      }
-      if (searchParams) {
-        endpoint = `${endpoint}${endpoint.includes('?') ? '&' : '?'}${new URLSearchParams(stringify(searchParams))}`;
-      }
-      do {
-        const response = await this.fetch(next, result ? undefined : init);
+          return populatedEndpoint;
+        },
+        nextEndpoint
+      );
+    }
+    if (params) {
+      init.body = new URLSearchParams(stringify(params));
+    }
+    if (searchParams) {
+      nextEndpoint = `${nextEndpoint}${nextEndpoint.includes('?') ? '&' : '?'}${new URLSearchParams(stringify(searchParams))}`;
+    }
+    do {
+      const response = await this.fetch(
+        nextEndpoint,
+        result ? undefined : init
+      );
+      if (response.ok) {
         const page = (await response.json()) as T;
         if (isError(page)) {
           throw new Error(
@@ -78,44 +83,61 @@ export class Client extends Canvas {
           const matches = /<([^>]+)>;\s*rel="next"/gm.exec(
             response!.headers.get('link') || ''
           );
-          next = matches && matches.length >= 2 ? matches[1] : undefined;
+          nextEndpoint =
+            matches && matches.length >= 2 ? matches[1] : undefined;
+        } else {
+          result = page;
         }
-      } while (Array.isArray(result) && next);
-  
-      if (result === undefined) {
+      } else {
         throw new Error(
-          `Unexpected undefined result: ${JSON.stringify({ endpoint, init: result ? undefined : init })}`
+          JSON.stringify(
+            {
+              url: response.url,
+              status: response.status,
+              statusText: response.statusText,
+              headers: response.headers,
+              response: response.text()
+            },
+            null,
+            2
+          )
         );
       }
-      return result;
+    } while (Array.isArray(result) && nextEndpoint);
+
+    if (result === undefined) {
+      throw new Error(
+        `Unexpected undefined result: ${JSON.stringify({ endpoint, init: result ? undefined : init })}`
+      );
     }
-  
-    public async get<T = JSONValue>(
-      endpoint: URL | RequestInfo,
-      init?: RequestInitMethod
-    ) {
-      return this.fetchAs<T>(endpoint, { ...init, method: 'GET' });
-    }
-  
-    public async post<T = JSONValue>(
-      endpoint: URL | RequestInfo,
-      init?: RequestInitMethod
-    ) {
-      return this.fetchAs<T>(endpoint, { ...init, method: 'POST' });
-    }
-  
-    public async put<T = JSONValue>(
-      endpoint: URL | RequestInfo,
-      init?: RequestInitMethod
-    ) {
-      return this.fetchAs<T>(endpoint, { ...init, method: 'PUT' });
-    }
-  
-    public async delete<T = JSONValue>(
-      endpoint: URL | RequestInfo,
-      init?: RequestInitMethod
-    ) {
-      return this.fetchAs<T>(endpoint, { ...init, method: 'DELETE' });
-    }
+    return result;
   }
-  
+
+  public async get<T = JSONValue>(
+    endpoint: URL | RequestInfo,
+    init?: RequestInitMethod
+  ) {
+    return this.fetchAs<T>(endpoint, { ...init, method: 'GET' });
+  }
+
+  public async post<T = JSONValue>(
+    endpoint: URL | RequestInfo,
+    init?: RequestInitMethod
+  ) {
+    return this.fetchAs<T>(endpoint, { ...init, method: 'POST' });
+  }
+
+  public async put<T = JSONValue>(
+    endpoint: URL | RequestInfo,
+    init?: RequestInitMethod
+  ) {
+    return this.fetchAs<T>(endpoint, { ...init, method: 'PUT' });
+  }
+
+  public async delete<T = JSONValue>(
+    endpoint: URL | RequestInfo,
+    init?: RequestInitMethod
+  ) {
+    return this.fetchAs<T>(endpoint, { ...init, method: 'DELETE' });
+  }
+}
