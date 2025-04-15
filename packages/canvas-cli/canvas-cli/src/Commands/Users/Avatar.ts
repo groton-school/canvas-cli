8683d28a17b29770e427e58b6de7417b860a2856
Seth Battis <sbattis@groton.org>
2025-04-15 15:41:10 -0400

    feat(canvas-cli): user avatars accept sis_user_id as well as user_id

--- packages/canvas-cli/canvas-cli/src/Commands/Users/Avatar.ts
+++ packages/canvas-cli/canvas-cli/src/Commands/Users/Avatar.ts
@@ -1,6 +1,7 @@
 import { Colors } from '@battis/qui-cli.colors';
 import { Core } from '@battis/qui-cli.core';
 import '@battis/qui-cli.env';
+import { Log } from '@battis/qui-cli.log';
 import * as Plugin from '@battis/qui-cli.plugin';
 import { Root } from '@battis/qui-cli.root';
 import * as Canvas from '@groton/canvas-cli.api';
@@ -9,7 +10,11 @@ import fs from 'node:fs';
 import path from 'node:path';
 import ora from 'ora';
 
-type Data = { user_id: string; path_to_avatar: string }[];
+type Data = {
+  user_id?: string;
+  sis_user_id?: string;
+  path_to_avatar: string;
+}[];
 
 export type Configuration = Plugin.Configuration & {
   csvPath?: string;
@@ -18,7 +23,7 @@ export type Configuration = Plugin.Configuration & {
 Core.configure({ core: { requirePositionals: true } });
 
 export const name = 'Users/Avatar';
-export const src = path.dirname(import.meta.dirname);
+export const src = path.resolve(import.meta.dirname, '../..');
 
 let csvPath: string | undefined = undefined;
 
@@ -30,7 +35,7 @@ export function options(): Plugin.Options {
   return {
     man: [
       {
-        text: `${Colors.value('arg0')} must be the path to a CSV file containing the columns ${Colors.quotedValue(`"user_id"`)} and ${Colors.quotedValue(`"path_to_avatar"`)}.`
+        text: `${Colors.value('arg0')} must be the path to a CSV file containing the columns ${Colors.quotedValue(`"user_id"`)} and/or ${Colors.quotedValue(`"sis_user_id"`)} and ${Colors.quotedValue(`"path_to_avatar"`)}.`
       }
     ]
   };
@@ -54,13 +59,36 @@ export async function run() {
       columns: true
     }
   );
-  for (const { user_id, path_to_avatar } of data) {
-    const spinner = ora(`User ID ${Colors.value(user_id)}`).start();
+
+  const users = await Canvas.v1.Accounts.Users.list({
+    // FIXME pagination on array response endpoints
+    // @ts-expect-error 2353 -- per_page should be declared in api
+    pathParams: { account_id: '1', per_page: 100 }
+  });
+
+  for (const { user_id: id, sis_user_id, path_to_avatar } of data) {
+    let user_id = id;
+    if (sis_user_id && !user_id) {
+      user_id = users
+        .find((user) => user.sis_user_id === sis_user_id)!
+        .id.toString();
+    }
+    const spinner = ora(
+      users.find((user) => user.id.toString() === user_id)?.name
+    ).start();
+    if (!user_id) {
+      spinner.fail(
+        `No Canvas ID found for sis_user_id ${Colors.value(sis_user_id)}`
+      );
+      continue;
+    }
     const file = await Canvas.upload({
-      pathParams: { user_id: user_id },
+      pathParams: { user_id },
       params: {
         name: 'avatar.jpg',
-        parent_folder_path: 'profile pictures'
+        parent_folder_path: 'profile pictures',
+        // @ts-ignore
+        as_user_id: user_id
       },
       localFilePath: path_to_avatar
     });
