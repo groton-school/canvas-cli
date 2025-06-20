9bb8e0893c397de24db1b6b0d3525f320827d0ef
Seth Battis <sbattis@groton.org>
2025-06-20 11:29:18 -0400

    refactor(canvas-cli.api): simplify re-exporting
    Take advantage of 611c7b0a292b5fb4a261ae3f7c61229d95bfc6ad

--- packages/canvas-cli/api/src/index.ts
+++ packages/canvas-cli/api/src/index.ts
@@ -2,8 +2,7 @@ import { PlannerItem as OverridePlannerItem } from './Overrides.js';
 import * as RenderedPlanner from './Resources/Planner.js';
 
 export * from './Client.js';
-export { Lti, Quiz, Sis, V1 as v1 } from './Endpoints/index.js';
-export * from './FileUploads.js';
+export * from './Endpoints/index.js';
 export * from './Resources/index.js';
 
 // inject undefined PlannerItem into Planner namespace
