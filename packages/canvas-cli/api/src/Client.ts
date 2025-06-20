7a0f63da0cb86fb0c998c50d2c89bdb73359e87a
Seth Battis <sbattis@groton.org>
2025-06-20 11:25:33 -0400

    feat(canvas-cli.api)!: externalize client management to client package
    BREAKING: Client management is no longer embedded in the API package,
    instead the API expects a call to `init()` providing an instance of
    `@groton/canvas-cli.client.base`'s `Client.Base` interface.

--- packages/canvas-cli/api/src/Client.ts
+++ packages/canvas-cli/api/src/Client.ts
@@ -1,53 +1,14 @@
-import { JSONObject, JSONValue } from '@battis/typescript-tricks';
-import * as Canvas from '@groton/canvas-cli.client';
+import * as Client from '@groton/canvas-cli.client.base';
 
-export interface Client {
-  instance_url: string;
-  fetchAs<T = JSONValue>(
-    endpoint: string | URL,
-    params?: {
-      pathParams: Record<string, string | number>;
-      searchParams: JSONObject;
-      params: JSONObject;
-    }
-  ): Promise<T>;
+let _client: Client.Base | undefined = undefined;
+
+export function init(client: Client.Base) {
+  _client = client;
 }
 
-function isClient(obj: object): obj is Client {
-  return 'fetchAs' in obj;
-}
-
-let _client: Client | undefined = undefined;
-
-export function init(client: Client): void;
-export function init(oauth2Credentials: Canvas.Credentials): void;
-export function init(arg: Canvas.Credentials | Client) {
-  if (isClient(arg)) {
-    _client = arg;
-  } else {
-    _client = new Canvas.Client(arg);
-  }
-}
-
-export function client() {
+export function client(): Client.Base {
   if (!_client) {
-    if (
-      process.env.CANVAS_CLIENT_ID &&
-      process.env.CANVAS_CLIENT_SECRET &&
-      process.env.CANVAS_INSTANCE_URL &&
-      process.env.CANVAS_REDIRECT_URI
-    ) {
-      init({
-        client_id: process.env.CANVAS_CLIENT_ID,
-        client_secret: process.env.CANVAS_CLIENT_SECRET,
-        instance_url: process.env.CANVAS_INSTANCE_URL,
-        redirect_uri: process.env.CANVAS_REDIRECT_URI,
-        store: process.env.CANVAS_TOKEN_STORE
-      });
-    }
-    if (!_client) {
-      throw new Error(`Canvas client has not been initialized`);
-    }
+    throw new Error('Client not initialized');
   }
   return _client;
 }
