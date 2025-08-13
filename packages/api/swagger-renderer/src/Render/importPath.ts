d4eaa53455490eb8ba966451287a99808c8461bf
Seth Battis <sbattis@groton.org>
2025-08-13 17:55:06 -0400

    fix: remove extraneous dot prefixes on relative paths

--- packages/api/swagger-renderer/src/Render/importPath.ts
+++ packages/api/swagger-renderer/src/Render/importPath.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 
 export function importPath(from: PathString, to: PathString) {
   let relative = path.relative(path.dirname(from), to);
-  if (!/\//.test(relative)) {
+  if (!/^\./.test(relative)) {
     relative = `./${relative}`;
   }
   relative = relative.replace(/\.ts$/, '.js');
