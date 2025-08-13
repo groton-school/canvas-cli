479605d7db4bb4fda07cab5ecef43b69ea660c44
Seth Battis <sbattis@groton.org>
2025-08-13 17:26:21 -0400

    feat: export Canvas and init()

--- packages/api/client/spa/src/index.ts
+++ packages/api/client/spa/src/index.ts
@@ -1,1 +1,9 @@
+import { init as apiInit } from '@groton/canvas-api';
+import { Client, Options } from './Client.js';
+
+export * as Canvas from '@groton/canvas-api';
 export * from './Client.js';
+
+export function init(options: Options) {
+  apiInit(new Client(options));
+}
