e5febe6d4acd754b895c695f2ff0549623cf4ee3
Seth Battis <sbattis@groton.org>
2026-02-16 09:26:49 -0500

    feat!: update to @oauth2-cli/qui-cli@0.6.0 with gcrtl support
    BREAKING: TokenStorage, FileStorage, and EnvironmentStorage are now
    inside the Token namespace.

--- packages/oauth2-cli/canvas/src/Canvas.ts
+++ packages/oauth2-cli/canvas/src/Canvas.ts
@@ -1,11 +1,7 @@
 import { CanvasPlugin } from './CanvasPlugin.js';
 
 export * from '@groton/canvas-api';
-export {
-  EnvironmentStorage,
-  FileStorage,
-  TokenStorage
-} from '@oauth2-cli/qui-cli/dist/OAuth2.js';
+export * as Token from '@oauth2-cli/qui-cli/dist/Token/index.js';
 export * from './CanvasPlugin.js';
 export * from './Client.js';
 
