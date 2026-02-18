f640f784f67381180d01be4240d2f6e6ff4baa48
Seth Battis <sbattis@groton.org>
2026-02-18 08:09:34 -0500

    chore: remove unused package

--- packages/msar/canvas-import-open/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Core } from '@qui-cli/core';
-import { register } from '@qui-cli/plugin';
-import * as CanvasImportOpen from './CanvasImportOpen.js';
-
-await register(CanvasImportOpen);
-await Core.run();
