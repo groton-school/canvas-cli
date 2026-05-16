44b7cb12f598ea8d03f4d4a424ac833bf9b949df
Seth Battis <sbattis@groton.org>
2026-05-16 14:58:18 -0400

    fix: rewrite canvas users avatar to be more reliable

--- packages/canvas-cli/src/Commands/Users/Avatar.ts
+++ packages/canvas-cli/src/Commands/Users/Avatar.ts
@@ -1,7 +1,6 @@
 import { Canvas } from '@oauth2-cli/canvas';
 import { Colors } from '@qui-cli/colors';
 import { Positionals } from '@qui-cli/core';
-import { Log } from '@qui-cli/log';
 import * as Plugin from '@qui-cli/plugin';
 import { Root } from '@qui-cli/root';
 import { parse } from 'csv-parse/sync';
@@ -36,7 +35,12 @@ export function options(): Plugin.Options {
   return {
     man: [
       {
-        text: `${Colors.positionalArg('arg0')} must be the path to a CSV file containing the columns ${Colors.quotedValue(`"user_id"`)} and/or ${Colors.quotedValue(`"sis_user_id"`)} and ${Colors.quotedValue(`"path_to_avatar"`)}.`
+        text:
+          `${Colors.positionalArg('arg0')} must be the path to a CSV file ` +
+          `containing the columns ${Colors.quotedValue(`"user_id"`)} and/or ` +
+          `${Colors.quotedValue(`"sis_user_id"`)} and absolute ` +
+          `${Colors.quotedValue(`"path_to_avatar"`)} (or relative to the CSV ` +
+          `file).`
       }
     ]
   };
@@ -54,66 +58,72 @@ export async function run() {
   if (!csvPath) {
     throw new Error(`${Colors.value('arg0')} CSV path must be defined`);
   }
-  try {
-    const data: Data = parse(
-      fs.readFileSync(path.resolve(Root.path(), csvPath)),
-      {
-        columns: true
-      }
+  const data: Data = parse(
+    fs.readFileSync(path.resolve(Root.path(), csvPath)),
+    {
+      columns: true
+    }
+  );
+
+  let spinner = ora(`Loading user list from Canvas`).start();
+  const users = await Canvas.v1.Accounts.Users.list({
+    path: { account_id: '1' },
+    query: { per_page: 100 }
+  });
+  spinner.succeed(`${users.length} Canvas users loaded`);
+
+  for (const { user_id, sis_user_id, path_to_avatar } of data) {
+    spinner = ora(`User ID ${Colors.value(user_id || sis_user_id)}`).start();
+    const user = users.find(
+      (user) => user.id == user_id || user.sis_user_id === sis_user_id
     );
-
-    let spinner = ora(`Loading user list from Canvas`).start();
-    const users = await Canvas.v1.Accounts.Users.list({
-      path: { account_id: '1' },
-      query: { per_page: 100 }
-    });
-    spinner.succeed(`${users.length} Canvas users loaded`);
-
-    for (const { user_id, sis_user_id, path_to_avatar } of data) {
-      spinner = ora(`User ID ${Colors.value(user_id || sis_user_id)}`).start();
-      const user = users.find(
-        (user) => user.id == user_id || user.sis_user_id === sis_user_id
+    if (!user) {
+      spinner.fail(
+        `No Canvas user found for user ID ${Colors.value(user_id || sis_user_id)}`
       );
-      if (!user) {
-        spinner.fail(
-          `No Canvas user found for user ID ${Colors.value(sis_user_id)}`
-        );
-        continue;
-      } else {
-        spinner.text = user.name;
-      }
-      try {
-        const file = await Canvas.v1.Users.Files.upload({
-          path: { user_id: user.id },
+      continue;
+    } else {
+      spinner.text = user.name;
+    }
+    const filePath = path.resolve(
+      Root.path(),
+      path.dirname(csvPath),
+      path_to_avatar
+    );
+    const name = `avatar${path.extname(path_to_avatar)}`.toLowerCase();
+    const file = await Canvas.v1.Users.Files.upload({
+      path: { user_id: user.id },
+      body: {
+        name,
+        parent_folder_path: 'profile pictures',
+        as_user_id: user.id
+      },
+      file: { filePath }
+    });
+    for (const avatar of await Canvas.v1.Users.Avatars.list({
+      path: { user_id: user.id }
+    })) {
+      if (avatar.display_name === file.display_name) {
+        await Canvas.v1.Users.update({
+          path: { id: user.id },
           body: {
-            name: 'avatar.jpg',
-            parent_folder_path: 'profile pictures',
-            as_user_id: user.id
-          },
-          file: { filePath: path_to_avatar }
+            'user[avatar][token]': avatar.token
+          }
         });
-        const avatars = await Canvas.v1.Users.Avatars.list({
-          path: { user_id: user.id }
+        await Canvas.v1.Users.update({
+          path: { id: user.id },
+          body: {
+            'user[avatar][state]': 'approved'
+          }
         });
-        const token = avatars.find(
-          // FIXME Avatars are sometimes Files
-          // @ts-expect-error 2339 bad spec from Instructure
-          (avatar) => 'uuid' in avatar && avatar.uuid === file.uuid
-        )?.token;
-        if (token) {
-          await Canvas.v1.Users.update({
-            path: { id: user.id },
-            body: { 'user[avatar][token]': token }
-          });
-          spinner.succeed();
-        } else {
-          spinner.fail();
-        }
-      } catch (error) {
-        Log.error(Colors.error((error as Error).message));
+        spinner.succeed(
+          `${Colors.value(user.name)}'s avatar set to ${Colors.path(path.basename(path_to_avatar))}`
+        );
+        break;
       }
     }
-  } catch (error) {
-    Log.error({ error });
+    if (spinner.isSpinning) {
+      spinner.fail(Colors.error(user.name));
+    }
   }
 }
