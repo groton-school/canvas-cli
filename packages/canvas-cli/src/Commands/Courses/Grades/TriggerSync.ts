f65483b80ff75a85c6719118414ddce0a2d6dea0
Seth Battis <sbattis@groton.org>
2026-09-05 20:18:25 -0400

    feat: write history of grade sync triggers to avoid repetitive calls

--- packages/canvas-cli/src/Commands/Courses/Grades/TriggerSync.ts
+++ packages/canvas-cli/src/Commands/Courses/Grades/TriggerSync.ts
@@ -1,4 +1,6 @@
-import { DateTimeString } from '@battis/descriptive-types';
+import fs from 'node:fs';
+import path from 'node:path';
+import { DateTimeString, PathString } from '@battis/descriptive-types';
 import { Canvas } from '@oauth2-cli/canvas';
 import { Colors } from '@qui-cli/colors';
 import * as Plugin from '@qui-cli/plugin';
@@ -7,6 +9,13 @@ import ora from 'ora';
 export type Configuration = Plugin.Configuration & {
   account?: number;
   due?: DateTimeString;
+  history?: PathString;
+};
+
+type HistoryEntry = {
+  timestamp: DateTimeString;
+  assignment: Canvas.Assignments.Assignment;
+  student: Canvas.Users.User;
 };
 
 const config: Configuration = { account: 1 };
@@ -37,6 +46,11 @@ export function options() {
       due: {
         description: 'When the trigger assignment should be due',
         default: config.due
+      },
+      history: {
+        description: `Path to history file to prevent re-triggering already triggered courses`,
+        env: 'GRADE_SYNC_TRIGGER_HISTORY',
+        default: config.history
       }
     }
   };
@@ -53,7 +67,25 @@ export async function run() {
   if (!config.due) {
     throw new Error(`${Colors.optionArg('due')} must be defined`);
   }
-  const spinner = ora('Loading active courses').start();
+
+  const spinner = ora();
+
+  let historyPath: string | undefined = undefined;
+  let history: Record<Canvas.Courses.Course['id'], HistoryEntry> = {};
+  if (config.history) {
+    historyPath = path.resolve(process.cwd(), config.history);
+    spinner.start(`Loading history from ${Colors.path(historyPath)}`);
+    if (fs.existsSync(historyPath)) {
+      history = JSON.parse(fs.readFileSync(historyPath, 'utf-8'));
+      spinner.succeed(
+        `${Object.keys(history).length} prior history entries loaded`
+      );
+    } else {
+      spinner.info('No prior history found');
+    }
+  }
+
+  spinner.start('Loading active courses');
   for (const course of await Canvas.v1.Accounts.Courses.list({
     path: { account_id: config.account },
     query: {
@@ -64,42 +96,60 @@ export async function run() {
     }
   })) {
     spinner.start(course.name);
-    const enrollment = (
-      await Canvas.v1.Courses.Users.list({
-        path: { course_id: course.id },
-        query: { enrollment_type: ['student'] }
-      })
-    ).shift();
-    if (enrollment) {
-      const trigger_assignment = await Canvas.v1.Courses.Assignments.create({
-        path: { course_id: course.id },
-        body: {
-          'assignment[name]': 'Grade Sync Trigger',
-          'assignment[notify_of_update]': false,
-          'assignment[submission_types]': ['none'],
-          'assignment[due_at]': config.due,
-          'assignment[points_possible]': 10,
-          'assignment[published]': true
-        }
-      });
-      spinner.text = `${spinner.text} / Assignment ${trigger_assignment.id}`;
-      spinner.text = `${spinner.text} / ${enrollment.name}`;
-      await Canvas.v1.Courses.Assignments.Submissions.grade_or_comment_on_submission_courses(
-        {
-          path: {
-            course_id: course.id,
-            assignment_id: trigger_assignment.id,
-            user_id: enrollment?.id
-          },
-          body: { 'submission[posted_grade]': '10' }
-        }
+    if (course.id in history) {
+      spinner.info(
+        `${Colors.value(course.name)} previously triggered ${history[course.id].timestamp}`
       );
-      await Canvas.v1.Courses.Assignments.delete_assignment({
-        path: { course_id: course.id, id: trigger_assignment.id }
-      });
-      spinner.succeed();
     } else {
-      spinner.fail();
+      const enrollment = (
+        await Canvas.v1.Courses.Users.list({
+          path: { course_id: course.id },
+          query: { enrollment_type: ['student'] }
+        })
+      ).shift();
+      if (enrollment) {
+        const trigger_assignment = await Canvas.v1.Courses.Assignments.create({
+          path: { course_id: course.id },
+          body: {
+            'assignment[name]': 'Grade Sync Trigger',
+            'assignment[notify_of_update]': false,
+            'assignment[submission_types]': ['none'],
+            'assignment[due_at]': config.due,
+            'assignment[points_possible]': 10,
+            'assignment[published]': true
+          }
+        });
+        spinner.text = `${spinner.text} / Assignment ${trigger_assignment.id}`;
+        spinner.text = `${spinner.text} / ${enrollment.name}`;
+        await Canvas.v1.Courses.Assignments.Submissions.grade_or_comment_on_submission_courses(
+          {
+            path: {
+              course_id: course.id,
+              assignment_id: trigger_assignment.id,
+              user_id: enrollment?.id
+            },
+            body: { 'submission[posted_grade]': '10' }
+          }
+        );
+        await Canvas.v1.Courses.Assignments.delete_assignment({
+          path: { course_id: course.id, id: trigger_assignment.id }
+        });
+        history[course.id] = {
+          timestamp: new Date().toISOString(),
+          assignment: trigger_assignment,
+          student: enrollment
+        };
+        if (historyPath) {
+          fs.writeFileSync(historyPath, JSON.stringify(history));
+        }
+        spinner.succeed();
+      } else {
+        spinner.fail();
+      }
     }
+  }
+
+  if (historyPath) {
+    spinner.succeed(`History written to ${Colors.path(historyPath)}`);
   }
 }
